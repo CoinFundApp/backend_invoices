88e424cddb53fc527bd82a94df856e00780957c6
noxPC <2914674+noxonsu@users.noreply.github.com>
2022-12-05 15:20:14 +0300

    initial

---
+++ src/config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  privateKey: {
+    mainnet: '',
+    testnet: '',
+  },
+  api: {
+    mainnet: 'https://insight.bitpay.com/api',
+    testnet: 'https://test-insight.swap.online/insight-api'
+  },
+  smsapi: ''
+}
